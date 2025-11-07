5c4a060c46f3b2d924cca11b8f89d5553b44c318
Itan Nikelskii <inikelsk@f0r3s26.codam.nl>
2025-11-07 11:28:17 +0100

    fixed a small norm issue introduced in my previous push

--- src/execution/executor.c
+++ src/execution/executor.c
@@ -6,7 +6,7 @@
 /*   By: inikelsk <inikelsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 10:23:14 by acossari          #+#    #+#             */
-/*   Updated: 2025/11/07 11:20:51 by inikelsk         ###   ########.fr       */
+/*   Updated: 2025/11/07 11:27:49 by inikelsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,7 @@ int	execute_single_command(t_command *cmd, t_shell *shell)
 {
 	int	exit_status;
 
-	 if (!cmd || !cmd->argv || !cmd->argv[0] || cmd->argv[0][0] == '\0')
+	if (!cmd || !cmd->argv || !cmd->argv[0] || cmd->argv[0][0] == '\0')
 		return (0);
 	if (!cmd->argv || !cmd->argv[0])
 	{
